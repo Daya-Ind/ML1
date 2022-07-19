1bac158ed9aba318110964379580fe30d9633c54
Daya-ind <daya.4047@gmail.com>
2022-07-19 18:45:21 +0530

    added base code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ML1
+create anaconda environment
+    conda create -p venv python==3.7 -y
+create a requirement file
+    pip install -r requirement.txt
+
+===========
+create app.py
---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+
+@app.route("/",methods=['Get','Post'])
+def index():
+    return "Flask running"
+
+if __name__=="__main__":
+    app.run(debug=True)
